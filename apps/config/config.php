107b4bccc925bbb3b7d39d0aac33aebf6c005082
developer03 <developer03@togu.co.id>
2021-03-29 22:43:17 +0700

    upload ui

--- apps/config/config.php
+++ apps/config/config.php
@@ -3,7 +3,7 @@
 define('BASE_URL', 'http://localhost:8888/pembangunan/public/');
 define('SITE_URL', 'http://localhost:8888/pembangunan/');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'usulan_pembangunan');
+define('DB_HOST', '203.161.184.104');
+define('DB_USER', 'penduduk_usulan_pembangunan');
+define('DB_PASS', 'usulan#penduduk');
+define('DB_NAME', 'penduduk_usulan_pembangunan');
